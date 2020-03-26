99a6607df2ca6e361321b594b09891cc4caef313
staicu adrian <adrian_staicu88@yahoo.com>
2020-03-26 23:00:31 +0200

    Add project files.

---
+++ store2/Produs.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace store2
+{
+    class Produs
+    {
+        public string nume { get; set; }
+        public double pret { get; set; }
+
+        public CategorieProdus electrocasniceMici { get; set; }
+
+        public Produs()
+        {
+            nume = " ";
+            pret = 0;
+            electrocasniceMici = new CategorieProdus(); 
+        }
+    }
+}
---
+++ store2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace store2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CategorieProdus electrocasniceMici = new CategorieProdus();
+            electrocasniceMici.denumireCategorie = "Electrocasnice mici";
+
+            Produs laptop = new Produs();
+            laptop.nume = "laptop Asus";
+            laptop.pret = 4500;
+            laptop.electrocasniceMici = electrocasniceMici;
+
+            Produs fiercalcat = new Produs();
+            fiercalcat.nume = " fier de calcat Philips";
+            fiercalcat.pret = 400;
+            fiercalcat.electrocasniceMici = electrocasniceMici;
+
+
+          
+            Console.WriteLine("Nume produs: " + laptop.nume + " Pret: " + laptop.pret + " Categorie: " + laptop.electrocasniceMici.denumireCategorie);
+            Console.WriteLine("Nume produs: " + fiercalcat.nume + " Pret: " + fiercalcat.pret + " Categorie: " + fiercalcat.electrocasniceMici.denumireCategorie); 
+
+            Console.ReadLine();
+        }
+    }
+}
